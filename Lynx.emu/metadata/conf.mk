0240060bbf63e42d9e1303093547b6a35029ae5e
k34o <140044976+k34o@users.noreply.github.com>
2025-07-10 15:37:49 +0900

    Delete Lynx.emu directory

--- Lynx.emu/metadata/conf.mk
+++
@@ -1,8 +0,0 @@
-include $(EMUFRAMEWORK_PATH)/metadata/conf.mk
-metadata_name = Lynx.emu
-metadata_exec = lynxemu
-metadata_pkgName = LynxEmu
-metadata_supportedFileExtensions += lnx lyx o
-metadata_id = com.explusalpha.LynxEmu
-metadata_vendor = Robert Broglia
-pnd_metadata_description = Lynx emulator using components from Mednafen
