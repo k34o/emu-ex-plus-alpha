ded5b37f85328a0f12ab421af40e7f7a70f85d72
k34o <140044976+k34o@users.noreply.github.com>
2025-07-10 15:40:13 +0900

    Delete NEO.emu directory

--- NEO.emu/metadata/conf.mk
+++
@@ -1,7 +0,0 @@
-include $(EMUFRAMEWORK_PATH)/metadata/conf.mk
-metadata_name = NEO.emu
-metadata_exec = neoemu
-metadata_pkgName = NeoEmu
-metadata_id = com.explusalpha.NeoEmu
-metadata_vendor = Robert Broglia
-pnd_metadata_description = Neo Geo emulator using components from Gngeo
