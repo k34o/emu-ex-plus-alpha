a877827ea317527c8852f45443d7cb6535b377da
k34o <140044976+k34o@users.noreply.github.com>
2025-07-10 15:39:20 +0900

    Delete NGP.emu directory

--- NGP.emu/metadata/conf.mk
+++
@@ -1,8 +0,0 @@
-include $(EMUFRAMEWORK_PATH)/metadata/conf.mk
-metadata_name = NGP.emu
-metadata_exec = ngpemu
-metadata_pkgName = NgpEmu
-metadata_supportedFileExtensions += ngc ngp npc
-metadata_id = com.explusalpha.NgpEmu
-metadata_vendor = Robert Broglia
-pnd_metadata_description = Neo Geo Pocket Color emulator using components from Mednafen
