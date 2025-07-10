be53fabc9c2316bef6e7262599c878fc4e981d5a
k34o <140044976+k34o@users.noreply.github.com>
2025-07-10 15:36:57 +0900

    Delete 2600.emu directory

--- 2600.emu/config.mk
+++
@@ -1,4 +0,0 @@
-ifndef EMUFRAMEWORK_PATH
- EMUFRAMEWORK_PATH := $(lastMakefileDir)/../EmuFramework
-endif
-metadata_confDeps = $(EMUFRAMEWORK_PATH)/metadata/conf.mk
--- 2600.emu/metadata/conf.mk
+++
@@ -1,8 +0,0 @@
-include $(EMUFRAMEWORK_PATH)/metadata/conf.mk
-metadata_name = 2600.emu
-metadata_exec = 2600emu
-metadata_pkgName = 2600Emu
-metadata_supportedFileExtensions += a26
-metadata_id = com.explusalpha.A2600Emu
-metadata_vendor = Robert Broglia
-pnd_metadata_description = Atari 2600 emulator using components from Stella
