c71e8878374630e25a0e65bc5355ffefef3f8b73
k34o <140044976+k34o@users.noreply.github.com>
2025-07-11 22:11:09 +0900

    Update conf.mk

--- Snes9x/metadata/conf.mk
+++ Snes9x/metadata/conf.mk
@@ -8,3 +8,4 @@ metadata_vendor = Robert Broglia
 pnd_metadata_license = Proprietary
 pnd_metadata_licenseURL = http://www.explusalpha.com/home/general-info/licenses/snes9x
 pnd_metadata_description = SNES/Super Famicom emulator using components from Snes9x
+android_metadata_versionCodeExtra = 1
