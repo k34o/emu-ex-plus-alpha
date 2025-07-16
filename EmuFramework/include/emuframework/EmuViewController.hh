7798085808da0e851b330b2c12d05cbd2712f633
k34o <140044976+k34o@users.noreply.github.com>
2025-07-16 14:22:53 +0900

    Update EmuViewController.hh

--- EmuFramework/include/emuframework/EmuViewController.hh
+++ EmuFramework/include/emuframework/EmuViewController.hh
@@ -43,7 +43,6 @@ using namespace IG;
 class EmuApp;
 class EmuAudio;
 class EmuSystem;
-class WramView;
 struct WindowData;
 struct FrameTimeConfig;
 class MainMenuView;
@@ -109,9 +108,6 @@ public:
 	EmuView emuView;
 	EmuInputView inputView;
 	ToastView popup;
-	#ifdef SNES9X_VERSION
-	std::unique_ptr<WramView> wramView;
-	#endif
 	ConditionalMember<Gfx::supportsPresentationTime, SteadyClockTimePoint> presentTime{};
 protected:
 	EmuMenuViewStack viewStack;
