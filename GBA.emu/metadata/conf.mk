0c39e9cab24eac76771ef559f61b9e9078daa590
k34o <140044976+k34o@users.noreply.github.com>
2025-08-30 11:18:52 +0900

    Delete GBA.emu directory

--- GBA.emu/metadata/conf.mk
+++
@@ -1,8 +0,0 @@
-include $(EMUFRAMEWORK_PATH)/metadata/conf.mk
-metadata_name = GBA.emu
-metadata_exec = gbaemu
-metadata_pkgName = GbaEmu
-metadata_supportedFileExtensions += gba
-metadata_id = com.explusalpha.GbaEmu
-metadata_vendor = Robert Broglia
-pnd_metadata_description = Gameboy Advance emulator using components from VBA-M
--- GBA.emu/src/core/gba/gbafilter.h
+++
@@ -1,5 +0,0 @@
-#include "../System.h"
-
-void gbafilter_pal(uint16_t* buf, int count, int redShift, int greenShift, int blueShift);
-void gbafilter_pal32(uint32_t* buf, int count, int redShift, int greenShift, int blueShift);
-void gbafilter_pad(uint8_t* buf, int count, int redShift, int greenShift, int blueShift);
