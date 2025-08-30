3692bd7848a867bf0fd7d38d82e3eb9b8b87a0fa
k34o <140044976+k34o@users.noreply.github.com>
2025-08-30 11:20:21 +0900

    Delete Saturn.emu directory

--- Saturn.emu/config.mk
+++
@@ -1,9 +0,0 @@
-ifndef EMUFRAMEWORK_PATH
- EMUFRAMEWORK_PATH := $(lastMakefileDir)/../EmuFramework
-endif
-metadata_confDeps = $(EMUFRAMEWORK_PATH)/metadata/conf.mk
-
-# A 64-bit device is needed for performance reasons
-android_minSDK = 21
-android_arch = arm64 x86_64
-ios_arch = arm64
--- Saturn.emu/metadata/conf.mk
+++
@@ -1,7 +0,0 @@
-include $(EMUFRAMEWORK_PATH)/metadata/conf.mk
-metadata_name = Saturn.emu
-metadata_exec = saturnemu
-metadata_pkgName = SaturnEmu
-metadata_supportedFileExtensions += cue ccd chd toc m3u
-metadata_id = com.explusalpha.SaturnEmu
-metadata_vendor = Robert Broglia
