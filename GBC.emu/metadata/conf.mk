8bbeb816f0b556465f1d277de5ff72f0277251c9
k34o <140044976+k34o@users.noreply.github.com>
2025-08-30 11:19:19 +0900

    Delete GBC.emu directory

--- GBC.emu/metadata/conf.mk
+++
@@ -1,8 +0,0 @@
-include $(EMUFRAMEWORK_PATH)/metadata/conf.mk
-metadata_name = GBC.emu
-metadata_exec = gbcemu
-metadata_pkgName = GbcEmu
-metadata_supportedFileExtensions += gb gbc dmg
-metadata_id = com.explusalpha.GbcEmu
-metadata_vendor = Robert Broglia
-pnd_metadata_description = Gameboy Color emulator using components from Gambatte
