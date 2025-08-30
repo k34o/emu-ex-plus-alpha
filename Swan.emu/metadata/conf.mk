17739c9bfb637377a317e0ac9248623617fa8e34
k34o <140044976+k34o@users.noreply.github.com>
2025-08-30 11:20:31 +0900

    Delete Swan.emu directory

--- Swan.emu/metadata/conf.mk
+++
@@ -1,8 +0,0 @@
-include $(EMUFRAMEWORK_PATH)/metadata/conf.mk
-metadata_name = Swan.emu
-metadata_exec = swanemu
-metadata_pkgName = SwanEmu
-metadata_supportedFileExtensions += ws wsc
-metadata_id = com.explusalpha.SwanEmu
-metadata_vendor = Robert Broglia
-pnd_metadata_description = WonderSwan emulator using components from Mednafen
