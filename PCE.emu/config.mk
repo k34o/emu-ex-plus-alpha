5ced86e382e2a95241360a93a908e5e52ba1e90c
k34o <140044976+k34o@users.noreply.github.com>
2025-08-30 11:20:10 +0900

    Delete PCE.emu directory

--- PCE.emu/config.mk
+++
@@ -1,8 +0,0 @@
-ifndef EMUFRAMEWORK_PATH
- EMUFRAMEWORK_PATH := $(lastMakefileDir)/../EmuFramework
-endif
-metadata_confDeps = $(EMUFRAMEWORK_PATH)/metadata/conf.mk
-
-# ld complains about ___floatdisf if using -dead_strip with LTO, cause unknown but could be linker bug
-# seems fixed in Xcode 5.1
-#ios_noDeadStrip := 1
